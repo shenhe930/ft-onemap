d042dec0c7d5c508b386fb3a5efa8e10a036c68b
shenhe930 <shenhe930@126.com>
2016-04-24 22:10:30 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,196 @@
+var fs = require('fs');
+var path = require('path');
+
+var gulp = require('gulp');
+
+// Load all gulp plugins automatically
+// and attach them to the `plugins` object
+var plugins = require('gulp-load-plugins')();
+
+// Temporary solution until gulp 4
+// https://github.com/gulpjs/gulp/issues/355
+var runSequence = require('run-sequence');
+
+var pkg = require('./package.json');
+var dirs = pkg['h5bp-configs'].directories;
+
+// ---------------------------------------------------------------------
+// | Helper tasks                                                      |
+// ---------------------------------------------------------------------
+
+gulp.task('archive:create_archive_dir', function () {
+    fs.mkdirSync(path.resolve(dirs.archive), '0755');
+});
+
+gulp.task('archive:zip', function (done) {
+
+    var archiveName = path.resolve(dirs.archive, pkg.name + '_v' + pkg.version + '.zip');
+    var archiver = require('archiver')('zip');
+    var files = require('glob').sync('**/*.*', {
+        'cwd': dirs.dist,
+        'dot': true // include hidden files
+    });
+    var output = fs.createWriteStream(archiveName);
+
+    archiver.on('error', function (error) {
+        done();
+        throw error;
+    });
+
+    output.on('close', done);
+
+    files.forEach(function (file) {
+
+        var filePath = path.resolve(dirs.dist, file);
+
+        // `archiver.bulk` does not maintain the file
+        // permissions, so we need to add files individually
+        archiver.append(fs.createReadStream(filePath), {
+            'name': file,
+            'mode': fs.statSync(filePath).mode
+        });
+
+    });
+
+    archiver.pipe(output);
+    archiver.finalize();
+
+});
+
+gulp.task('clean', function (done) {
+    require('del')([
+        dirs.archive,
+        dirs.dist
+    ]).then(function () {
+        done();
+    });
+});
+
+gulp.task('copy', [
+    'copy:index.html',
+    'copy:jquery',
+    'copy:bootstrap',
+    'copy:misc'
+]);
+
+
+gulp.task('copy:index.html', function () {
+    return gulp.src(dirs.src + '/index.html')
+               .pipe(plugins.replace(/{{JQUERY_VERSION}}/g, pkg.devDependencies.jquery))
+               .pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('copy:jquery', function () {
+    return gulp.src(['node_modules/jquery/dist/jquery.min.js'])
+               .pipe(plugins.rename('jquery-' + pkg.devDependencies.jquery + '.min.js'))
+               .pipe(gulp.dest(dirs.dist + '/js/vendor'));
+});
+
+gulp.task('copy:bootstrap',[
+    'copy:bootstrap:css',
+    'copy:bootstrap:js',
+    'copy:bootstrap:fonts'
+]);
+
+gulp.task('copy:bootstrap:js', function () {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js'])
+        .pipe(gulp.dest(dirs.dist + '/js/vendor'));
+});
+
+gulp.task('copy:bootstrap:css', function () {
+    return gulp.src([
+        'node_modules/bootstrap/dist/css/bootstrap.css',
+        'node_modules/bootstrap/dist/css/bootstrap.min.css'])
+        .pipe(gulp.dest(dirs.dist + '/css'));
+});
+
+gulp.task('copy:bootstrap:fonts', function () {
+    return gulp.src(['node_modules/bootstrap/dist/fonts/*.*'])
+               .pipe(gulp.dest(dirs.dist + '/fonts'));
+});
+
+
+gulp.task('copy:misc', function () {
+    return gulp.src([
+
+        // Copy all files
+        dirs.src + '/**/*',
+
+        // Exclude the following files
+        // (other tasks will handle the copying of these files)
+        '!' + dirs.src + '/index.html'
+
+    ], {
+
+        // Include hidden files by default
+        dot: true
+
+    }).pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('copy:js', function () {
+    return gulp.src(dirs.src+'/js/**/*.js')
+        .pipe(gulp.dest(dirs.dist + '/js'));
+});
+
+gulp.task('lint:js', function () {
+    return gulp.src([
+        'gulpfile.js',
+        dirs.src + '/js/*.js'
+    ]).pipe(plugins.jscs())
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter('jshint-stylish'))
+      .pipe(plugins.jshint.reporter('fail'));
+});
+
+gulp.task('less', function () {
+    var banner = '/*! ft-onemap v' + pkg.version +
+        ' | ' + pkg.license.type + ' License' +
+        ' | ' + pkg.homepage + ' */\n\n';
+
+    gulp.src(dirs.src+'/less/main.less')
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.less())
+        .pipe(plugins.header(banner))
+        .pipe(plugins.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 8', '> 1%'],
+            cascade: true
+        }))
+        .pipe(plugins.sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.dist+'/css'));
+});
+
+// ---------------------------------------------------------------------
+// | Main tasks                                                        |
+// ---------------------------------------------------------------------
+
+gulp.task('archive', function (done) {
+    runSequence(
+        'build',
+        'archive:create_archive_dir',
+        'archive:zip',
+    done);
+});
+
+gulp.task('build', function (done) {
+    runSequence(
+        ['clean', 'lint:js'],
+        'less',
+        'copy',
+    done);
+});
+
+gulp.task('default', ['build']);
+
+// ---------------------------------------------------------------------
+// | Watch tasks                                                        |
+// ---------------------------------------------------------------------
+gulp.task('watch',function () {
+    gulp.watch(dirs.src + '/index.html', ['copy:index.html']);
+
+    gulp.watch(dirs.src + '/less/**/*.less', ['less']);
+
+    gulp.watch([dirs.src + '/**/*.js','gulpfile.js'],['lint:js','copy:js']);
+});
+
